6b52470d4804558b9f5dd58b8842abacab5b1860
mfdiaz <105811825+mfdiaz308@users.noreply.github.com>
2023-12-19 11:16:41 +0100

    Created index.html, added search bar to web page
    No searching functionality yet

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, render_template, request
 import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_query = request.form['search']
+        print("Search query:", search_query)
+        # Redirects back to the home page for now
+        return render_template('index.html')
+    # If it's a GET request or other method, redirect to the home page
+    return render_template('index.html')
 
 @app.route('/', methods=['GET'])
 def hiWorld():
