9f6cb96dcf71bdb3e68fbd35dc1abbd7dd3dce46
mfdiaz <105811825+mfdiaz308@users.noreply.github.com>
2023-12-20 21:11:42 +0100

    Search bar displays table, dynamic cells
    Not actual results yet

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
 from flask import Flask, render_template, request
-import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,13 +14,9 @@ def search():
         search_query = request.form['search']
         print("Search query:", search_query)
         # Redirects back to the home page for now
-        return render_template('index.html')
+        return render_template('results.html', search_input = search_query)
     # If it's a GET request or other method, redirect to the home page
-    return render_template('index.html')
-
-@app.route('/', methods=['GET'])
-def hiWorld():
-    return 'Hi World\n'
+    return render_template('index.html', search_input = '')
 
 if __name__ == '__main__':
     app.run(debug=True)
