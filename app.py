ada72e900efb80a2f329404912267034eb809a61
mfdiaz <105811825+mfdiaz308@users.noreply.github.com>
2023-12-21 11:31:01 +0100

    Added test json, dynamic web page

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
+import json
 from flask import Flask, render_template, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+def get_course(query):
+    with open('test.json','r') as f:
+        data = json.load(f)
+
+    for course in data:
+        if query.lower() in course['name'].lower() or query.lower() in course['skills'] or query.lower() in course['description']:
+            print(str(course['name']))
+            return course
+    
+    return None
 
 @app.route('/')
 def index():
@@ -13,10 +25,15 @@ def search():
     if request.method == 'POST':
         search_query = request.form['search']
         print("Search query:", search_query)
+        course = get_course(search_query)
         # Redirects back to the home page for now
-        return render_template('results.html', search_input = search_query)
+        return render_template('results.html', course_url = course['url'], course_name = course['name'])
     # If it's a GET request or other method, redirect to the home page
     return render_template('index.html', search_input = '')
+
+#TODO: setup apache server
+#TODO: put all json files in one file
+#TODO: display search_query directly when switching to results.html
 
 if __name__ == '__main__':
     app.run(debug=True)
